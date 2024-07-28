7358a792b96cc70f9fc18b20c30c4d101277dd46
zhou-ray <52714533+zhou-ray@users.noreply.github.com>
2024-07-28 17:10:00 -0500

    Initial

---
+++ CarSales.py
@@ -0,0 +1,104 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (to_timestamp, col, lower, expr,
+                                   regexp_replace, regexp_extract, concat)
+from pyspark.sql.types import StringType
+import mysql.connector
+from mysql.connector import Error
+
+spark = (SparkSession.builder.appName("CarSales")
+         .config("spark.driver.extraClassPath", "mysql-connector-j-9.0.0\mysql-connector-j-9.0.0.jar")
+         .getOrCreate())
+
+# Load in both csv files
+carinfo_df = spark.read.csv("carinfo.csv", header=True, inferSchema=True)
+saleinfo_df = spark.read.csv("saleinfo.csv", header=True, inferSchema=True)
+
+"""
+Data Cleaning
+"""
+# Remove rows with no vin number
+carinfo_df = carinfo_df.dropna(subset=['vin'])
+saleinfo_df = saleinfo_df.dropna(subset=['vin'])
+
+# Remove Duplicates
+carinfo_df = carinfo_df.dropDuplicates()
+saleinfo_df = saleinfo_df.dropDuplicates()
+
+# Lowercase all text
+for c in carinfo_df.columns:
+    if carinfo_df.schema[c].dataType == StringType():
+        carinfo_df = carinfo_df.withColumn(c,lower(carinfo_df[c]))
+for c in saleinfo_df.columns:
+    if saleinfo_df.schema[c].dataType == StringType():
+        saleinfo_df = saleinfo_df.withColumn(c,lower(saleinfo_df[c]))
+
+# Consolidate alternate spellings of make
+# correct_brands = ['acura', 'airstream', 'aston martin', 'audi', 'bentley', 'bmw', 'buick', 'cadillac', 'chevrolet',
+#                   'chrysler', 'daewoo', 'dodge', 'dot', 'ferrari', 'fiat', 'fisker', 'ford', 'geo', 'gmc', 'honda',
+#                   'hummer', 'hyundai', 'infiniti', 'isuzu', 'jaguar', 'jeep', 'kia', 'lamborghini', 'land rover',
+#                   'landrover', 'lexus', 'lincoln', 'lotus', 'maserati', 'mazda', 'mercedes-benz', 'mercury', 'mini',
+#                   'mitsubishi', 'nissan', 'oldsmobile', 'plymouth', 'pontiac', 'porsche', 'ram', 'rolls-royce', 'saab',
+#                   'saturn', 'scion', 'smart', 'subaru', 'suzuki', 'tesla', 'toyota', 'volkswagen', 'volvo']
+
+corrections = {
+    "chev truck": "chevrolet",
+    "dodge tk": "dodge",
+    "ford tk": "ford",
+    "ford truck": "ford",
+    "gmc truck": "gmc",
+    "hyundai tk": "hyundai",
+    "landrover": "land rover",
+    "mazda tk": "mazda",
+    "mercedes": "mercedes-benz",
+    "mercedes-b": "mercedes-benz",
+    "vw": "volkswagen"
+}
+
+carinfo_df = carinfo_df.replace(corrections, subset=["make"])
+# Check the schema of both dfs
+print(carinfo_df.schema)
+print(saleinfo_df.schema)
+carinfo_df.show()
+# Rework the saledate column
+# Convert to timestamp and standardize all time to UTC/GMT+0
+saleinfo_df = (saleinfo_df.withColumn('saledate', regexp_replace(col('saledate'), r'\b(?:mon |tue |wed |thu |fri |sat |sun )\b', ""))
+               .withColumn('gmt_direction', regexp_extract(col('saledate'), r'([+-])', 0))
+               .withColumn('gmt_hours', concat(col('gmt_direction'),regexp_extract(col('saledate'), r'[+-](\d{2})', 1)).cast('integer'))
+               .withColumn('gmt_minutes', concat(col('gmt_direction'),regexp_extract(col('saledate'), r'[+-]\d{2}(\d{2})', 1)).cast('integer'))
+               .withColumn('gmt_minutes', (col('gmt_hours') * 60) + col('gmt_minutes'))
+               .withColumn('saledate', regexp_replace(col('saledate'), r'( gmt).*',""))
+               .withColumn('saledate', to_timestamp(col('saledate'),"MMM dd yyyy HH:mm:ss"))
+               .withColumn('saledate', expr('saledate - INTERVAL 1 MINUTES * gmt_minutes'))
+               )
+saleinfo_df = saleinfo_df.drop(*['gmt_direction','gmt_hours','gmt_minutes'])
+
+
+carsales_df = saleinfo_df.join(carinfo_df, 'vin')
+carsales_df.show(truncate=False)
+
+mysql_properties = {
+    "user": "ray",
+    "password": "admin",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+
+# # Create the mySQL server
+try:
+    connection = mysql.connector.connect(host='127.0.0.1',user='ray',password='admin')
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE DATABASE IF NOT EXISTS cars')
+        print('Database creation Successful')
+
+except Error as e:
+    print(f"Error: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+
+carsales_df.write.jdbc(url="jdbc:mysql://127.0.0.1:3306/cars",table='data',mode='append',properties=mysql_properties)
+
+# Run some basic corrections on the MySQL server
---
+++ SQLCorrections.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='127.0.0.1',user='ray',password='admin',database='cars')
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        create_table = """
+        CREATE TABLE IF NOT EXISTS valid_states (
+            abrv VARCHAR(2),
+            name VARCHAR(255)
+            )
+            """
+        cursor.execute(create_table)
+        insert_query = "INSERT INTO valid_states (abrv,name) VALUES (%s, %s)"
+        # including provinces
+        state_list = [
+            ('al','alabama'),('ak','alaska'),('az','arizona'),('ar','arkansas'),('ca','california'),('co','colorado'),
+            ('ct','connecticut'),('de','delaware'),('fl','florida'),('ga','georgia'),('hi','hawaii'),('id','idaho'),
+            ('il','illinois'),('in','indiana'),('ia','iowa'),('ks','kansas'),('ky','kentucky'),('la','louisiana'),
+            ('me','maine'),('md','maryland'),('ma','massachusetts'),('mi','michigan'),('mn','minnesota'),
+            ('ms','mississippi'),('mo','missouri'),('mt','montana'),('ne','nebraska'),('nv','nevada'),
+            ('nh','new hampshire'),('nj','new jersey'),('nm','new mexico'),('ny','new york'),('nc','north carolina'),
+            ('nd','north dakota'),('oh','ohio'),('ok','oklahoma'),('or','oregon'),('pa','pennsylvania'),
+            ('ri','rhode island'),('sc','south carolina'),('sd','south dakota'),('tn','tennessee'),('tx','texas'),
+            ('ut','utah'),('vt','vermont'),('va','virginia'),('wa','washington'),('wv','west virginia'),
+            ('wi','wisconsin'),('wy','wyoming'),('ab','alberta'),('bc','british columbia'),('mb','manitoba'),
+            ('nb','new brunswick'),('nf','newfoundland'),('nt','northwest territories'),('ns','nova scotia'),
+            ('nu','nunavut'),('on','ontario'),('pe','prince edward island'),('qc','quebec'),('sk','saskatchewan'),
+            ('yt','yukon')
+        ]
+
+        # sts = [(st,) for st in state_list]
+        cursor.executemany(insert_query, state_list)
+
+        delete_query = """
+        DELETE FROM data 
+        WHERE state NOT IN (SELECT abrv FROM valid_states)
+        """
+        cursor.execute(delete_query)
+        print('Successful')
+        connection.commit()
+
+except Error as e:
+    print(f"Error: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
